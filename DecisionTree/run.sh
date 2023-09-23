a96b450100ced9ff23d46192fad808b020a35dfb
Pravallika Abbineni <68220390+pravallika10473@users.noreply.github.com>
2023-09-22 23:24:37 -0600

    Add files via upload
    run.sh file

---
+++ DecisionTree/run.sh
@@ -0,0 +1,10 @@
+#/bin/sh
+
+echo "Training and Test errors for car dataset"
+python3 decissiontreecar.py
+
+echo "Results for bank dataset -unknown as feature value"
+python3 DecisionTree_bank.py
+
+echo "Results for bank dataset -unknown as missing"
+python3 DecisionTree_bank_missing.py
